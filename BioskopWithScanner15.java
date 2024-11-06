624633dac651087db8d510eee6c4fae8c987c540
Hikmal7cyber <mhikmal.ag@gmail.com>
2024-11-06 22:00:16 +0700

    Bioskop With Scanner Done

--- BioskopWithScanner15.java
+++ BioskopWithScanner15.java
@@ -7,7 +7,7 @@ public class BioskopWithScanner15 {
         int baris, kolom;
 
         while (true) {
-            System.out.println("Pilihan menu bioskop 'DASPRO'");
+            System.out.println("====== Pilihan menu bioskop 'DASPRO' ======");
             System.out.println("1. Input data penonton");
             System.out.println("2. Daftar Penonton");
             System.out.println("3. exit");
@@ -18,6 +18,8 @@ public class BioskopWithScanner15 {
         
             if (pilihan == 1){
                 while (true){
+                    System.out.println();
+                    System.out.println("------ IDENTITAS PENONTON ------");
                     System.out.print("Masukkan nama: ");
                     nama = sc.nextLine();
                     System.out.print("Masukkan baris (1-4): ");
@@ -25,6 +27,18 @@ public class BioskopWithScanner15 {
                     System.out.print("Masukkan kolom (1-2): ");
                     kolom = sc.nextInt();
                     sc.nextLine();
+
+                    if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                        System.out.println("Input baris atau kolom tidak valid!");
+                    } else {
+                        if (penonton[baris - 1][kolom - 1] == null) {
+                            penonton[baris - 1][kolom - 1] = nama;
+                            System.out.println("Data penonton sukses disimpan!");
+                            break;
+                        } else {
+                            System.out.println("Tempat sudah terisi, silakan pilih tempat lain.");
+                        }
+                    }
 
                     penonton[baris-1][kolom-1] = nama;
 
@@ -35,15 +49,22 @@ public class BioskopWithScanner15 {
                     break;
                     }
                 }
+
             }if (pilihan == 2){
-                System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
-                System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
-                System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
-                System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+                System.out.println();
+                System.out.println("------ DENAH KURSI PENONTON ------");
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Penonton pada baris ke-" + (i + 1) + ": ");
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        System.out.print((penonton[i][j] != null ? penonton[i][j] : "***") + "\t");
+                    }
+                    System.out.println();
+                }
+
             } if (pilihan == 3) {
                 break;
             }
         }
-        System.out.println("Bioskop telah tutup");
+        System.out.println("====== Bioskop telah tutup ======");
     }
 }
